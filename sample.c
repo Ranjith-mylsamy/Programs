19e33382255b53d573516fe3dc2d57050631b5ac
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-10-10 00:15:44 +0530

    pointers

--- sample.c
+++ sample.c
@@ -1,49 +1,14 @@
-#include<stdio.h>    
-#include<stdlib.h>  
-int main(){  
-int a[10][10],b[10][10],mul[10][10],r,c,i,j,k;    
-system("cls");  
-printf("enter the number of row=");    
-scanf("%d",&r);    
-printf("enter the number of column=");    
-scanf("%d",&c);    
-printf("enter the first matrix element=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&a[i][j]);    
-}    
-}    
-printf("enter the second matrix element=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-scanf("%d",&b[i][j]);    
-}    
-}    
-    
-printf("multiply of the matrix=\n");    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-mul[i][j]=0;    
-for(k=0;k<c;k++)    
-{    
-mul[i][j]+=a[i][k]*b[k][j];    
-}    
-}    
-}    
-//for printing result    
-for(i=0;i<r;i++)    
-{    
-for(j=0;j<c;j++)    
-{    
-printf("%d\t",mul[i][j]);    
-}    
-printf("\n");    
-}    
-return 0;  
+#include<stdio.h>
+int main(){
+    int *ptr1;
+    int **ptr2;
+    int x=10;
+    printf("The normal value is:%d\n",ptr1);
+    printf("the value is:%p\n",ptr1); //address of ptr1
+    ptr1=&x;
+    ptr2=&ptr1;
+    printf("The value is:%d\n",ptr1); //address of x in terms of integer value
+    printf("the value is:%d\n",*ptr1); //value stored in ptr1
+    printf("the value is:%p\n",ptr1); //address of ptr1
+    printf("the double pointer value is:%d\n",**ptr2); //value stored in ptr2
 }
