4658d01f54b9e48954217da9c5aaa6300c656137
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-10-15 09:46:26 +0530

    palindrome integer [leetcode]

---
+++ leetcode/palindrome_number.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int reversefunction(int num)
+{
+    int currentdigit,prev_revnum=0;
+    int rev_num=0;
+    while(num!=0)
+    {
+        currentdigit=num%10;
+        rev_num=(rev_num*10)+currentdigit;
+        if((rev_num - currentdigit)/10 != prev_revnum)
+        {
+            printf("overflowed");
+            return 0;
+        }
+        prev_revnum=rev_num;
+        num/=10;
+    }
+    return rev_num;
+}
+int main(){
+    int a;
+    printf("Enter the number you want to reverse\n");
+    scanf("%d",&a);
+    int result=reversefunction(a);
+    if(a==result)
+    {
+        printf("true");
+    }
+    else
+    {
+        printf("false");
+    }
+    return 1;
+}
