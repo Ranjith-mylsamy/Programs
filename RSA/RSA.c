97b0cd2cc728d54dc9182d3db04de7892762524e
Ranjith-mylsamy <ranjithmylsamy@gmail.com>
2022-11-22 23:20:13 +0530

    RSA algorithm prime number

---
+++ RSA/RSA.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+#include<stdlib.h>
+int prime(long int pr)
+{
+    int j=pr/2;
+    for(int i=2;i<=j;i++)
+    {
+        if(pr%2==0)
+        {
+            return 0;
+        }
+        else
+        {
+            return 1;
+        }
+    }
+}
+int main()
+{
+    int p,q,flag;
+    printf("Enter first prime numbers\n");
+    scanf("%d",&p);
+    flag=prime(p);
+    if(flag==0)
+    {
+        printf("It is not a prime number\n");
+        exit(1);
+    }
+    printf("Enter Second prime number\n");
+    scanf("%d",&q);
+        flag=prime(q);
+    if(flag==0)
+    {
+        printf("It is not a prime number\n");
+        exit(1);
+    }
+}
